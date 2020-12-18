06f210fd28e5e7312ab732bc71f59d8b74e9b1bb
nils <nils.bohland@icloud.com>
2020-12-18 11:18:03 +0100

    testevent erstellt

--- src/createEvents/pushEvents.java
+++ src/createEvents/pushEvents.java
@@ -1,9 +1,15 @@
 package createEvents;
 import Constants.*;
+import com.sun.java.accessibility.util.EventQueueMonitor;
+
 import java.util.ArrayList;
 
 public class pushEvents {
-    public ArrayList<Event> getEvents(Topic t){
+    public static ArrayList<Event> getEvents(Topic t){
+        ArrayList<Event> events = new ArrayList<Event>();
+        events.add(new Event(Type.EXPERIMENT, "description", new Solution(), new Solution(), new Solution()));
+        events.add(new Event(Type.QUESTION, "description2", new Solution(), new Solution(), new Solution()));
+        events.add(new Event(Type.STORYELEMENT, "description3", new Solution(), new Solution(), new Solution()));
         return new ArrayList<Event>();
     }
 }
