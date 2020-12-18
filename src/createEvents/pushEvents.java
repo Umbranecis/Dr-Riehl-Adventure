b3e3ec99f509429febcf41a0f665819521fa565a
nils <nils.bohland@icloud.com>
2020-12-18 11:18:49 +0100

    testevent erstellt

--- src/createEvents/pushEvents.java
+++ src/createEvents/pushEvents.java
@@ -10,6 +10,6 @@ public class pushEvents {
         events.add(new Event(Type.EXPERIMENT, "description", new Solution(), new Solution(), new Solution()));
         events.add(new Event(Type.QUESTION, "description2", new Solution(), new Solution(), new Solution()));
         events.add(new Event(Type.STORYELEMENT, "description3", new Solution(), new Solution(), new Solution()));
-        return new ArrayList<Event>();
+        return events;
     }
 }
