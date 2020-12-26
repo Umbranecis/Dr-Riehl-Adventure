eabebc4d396784b632f57689ca5eddfe30a77586
nils <nils.bohland@icloud.com>
2020-12-26 04:23:59 +0100

    soweit fertig, txts müssen noch gemacht werden

--- src/Constants/Topic.java
+++ src/Constants/Topic.java
@@ -1,17 +1,19 @@
 package Constants;
 
 public enum Topic {
-    MAGNETFELD("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/Magnetfeld"){},
-    LORENTZKRAFT("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/Lorentzkraft"){},
-    OHMSCHESGESETZ("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/OhmschesGesetz"){},
-    ELEKTROMAGNETISMUS("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/Elektromagnetismus"){},
-    MAGNETISCHEMONOPOLE("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/MagnetischeMonopole"){};
+    MAGNETFELD("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/Magnetfeld", "Magnetfeld"){},
+    LORENTZKRAFT("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/Lorentzkraft", "Lorentzkraft"){},
+    OHMSCHESGESETZ("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/OhmschesGesetz","Ohmsches Gesetz"){},
+    ELEKTROMAGNETISMUS("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/Elektromagnetismus", "Elektromagnetismus"){},
+    MAGNETISCHEMONOPOLE("/Users/nils/Desktop/java/Dr Riehl Adventure/src/txtFiles/MagnetischeMonopole", "Magnetische Monopole"){};
 
 
-    Topic(String path){
+    Topic(String path, String callUpString){
         this.path = path;
+        this.callUpString = callUpString;
     }
     public String path;
+    public String callUpString;
 
 
 
--- src/createEvents/ImageManager.java
+++ src/createEvents/ImageManager.java
@@ -1,10 +1,27 @@
 package createEvents;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public abstract class ImageManager {
+    //Ordner der Bilder
+    String imagePath = "/images";
+    // Datei für die Verwaltung
+    String connectionPath = "";
     static HashMap<String, String> images = new HashMap<String, String>();
 
     public static String getImagePath(Event e){
         return images.get(e.convertToString());
+    }
+
+    public static void fillMap(){
+        Scanner imageConnections = null;
+        try {
+            imageConnections = new Scanner(new File("/images/ImageConections"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
